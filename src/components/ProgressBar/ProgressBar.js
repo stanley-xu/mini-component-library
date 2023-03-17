3f40cb4bab4fd3ecc37783a103adac15c92a23d4
Stanley Xu <stanley.xuu@gmail.com>
2023-03-17 18:17:05 -0400

    approach 1: switch and compose base

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,57 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Base = styled.div`
+  background-color: ${COLORS.transparentGray15};
+`
+
+const Small = styled(Base)`
+  height: 8px;
+  border-radius: 4px;
+`
+
+const Medium = styled(Base)`
+  height: 12px;
+  border-radius: 4px;
+`
+
+const Large = styled(Base)`
+  height: 24px;
+  border-radius: 8px;
+`
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let Wrapper;
+  switch (size) {
+    case 'small': Wrapper = Small; break;
+    case 'medium': Wrapper = Medium; break;
+    case 'large': Wrapper = Large; break;
+    default: Wrapper = Medium
+  }
+
+  const accessibleContent = (
+    <VisuallyHidden>
+      <strong>{value}</strong>
+    </VisuallyHidden>
+  );
+
+  const MeterWrapper = styled.div`
+    width: ${value}%;
+    height: 100%;
+    background-color: ${COLORS.primary};
+    border-radius: inherit;
+  `
+
+  const markup = (
+    <Wrapper>
+      <MeterWrapper>
+        {accessibleContent}
+
+      </MeterWrapper>
+    </Wrapper>
+  )
+
+  return markup
 };
 
 export default ProgressBar;
