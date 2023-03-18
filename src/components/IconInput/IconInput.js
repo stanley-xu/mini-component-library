1cccf1d6d05bce45f398242e8fd3c84df2c7e021
Stanley Xu <stanley.xuu@gmail.com>
2023-03-18 01:05:27 -0400

    iconinput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,80 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Wrapper = styled.div`
+  position: relative;
+
+  &:hover {
+    & svg, & input {
+      color: black;
+    }
+  }
+`
+
+const BaseIconWrapper = styled.span`
+  & svg {
+    color: ${COLORS.gray700};
+  }
+`
+
+const SmallIconWrapper = styled(BaseIconWrapper)`
+  position: absolute;
+  left: 2px;
+  top: 4px;
+  // My hack to make this icon clickable
+  // You're in fact clicking <Input />, it's presented above the icon
+  z-index: -1;
+`
+
+const LargeIconWrapper = styled(BaseIconWrapper)`
+  position: absolute;
+  left: 2px;
+  top: 6px;
+  // My hack to make this icon clickable
+  // You're in fact clicking <Input />, it's presented above the icon
+  z-index: -1;
+`
+
+const Input = ({ width, ...rest }) => {
+  const Component = styled.input`
+    padding-left: var(--spacing-left);
+    padding-bottom: var(--spacing-bottom);
+    padding-top: var(--spacing-top);
+    width: ${width}px;
+
+    background-color: transparent;
+
+    border: none;
+    border-bottom: 2px solid ${COLORS.black};
+
+    font-family: Roboto, sans-serif;
+    font-size: var(--font-size);
+    font-weight: 700;
+    color: ${COLORS.gray700};
+    &::placeholder {
+      font-weight: 400;
+      color: ${COLORS.gray500};
+    }
+  `
+
+  return <Component {...rest} />
+}
+
+const INPUT_VARIANT_PROPERTIES = {
+  small: {
+    '--font-size': '14px',
+    '--spacing-left': '24px',
+    '--spacing-bottom': '4px',
+    '--spacing-top': '4px',
+  },
+  large: {
+    '--font-size': '18px',
+    '--spacing-left': '36px',
+    '--spacing-bottom': '6px',
+    '--spacing-top': '8px',
+  },
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +87,38 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
-};
+  const inputVariantProps = INPUT_VARIANT_PROPERTIES[size]
+  if (!inputVariantProps) throw new Error(`Unknown size variant: ${size}`)
+
+  let iconMarkup;
+  switch (size) {
+    case 'small': {
+      iconMarkup = (
+        <SmallIconWrapper>
+          <Icon id={icon} size={16} strokeWidth={2} />
+        </SmallIconWrapper>
+      )
+      break;
+    }
+    case 'large': {
+      iconMarkup = (
+        <LargeIconWrapper>
+          <Icon id={icon} size={24} strokeWidth={2} />
+        </LargeIconWrapper>
+      )
+      break;
+    }
+    default: iconMarkup = null
+  }
+
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      {iconMarkup}
+      <Input width={width} placeholder={placeholder} style={inputVariantProps} />
+    </Wrapper>
+  )
+}
 
 export default IconInput;
